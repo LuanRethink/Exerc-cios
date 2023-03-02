f2702ab657b57e2155b8efc357e75e5a0fa28797
LuanRethink <124921888+LuanRethink@users.noreply.github.com>
2023-03-02 08:14:33 -0300

    mudei o return

--- js_objetcs/ex.js
+++ js_objetcs/ex.js
@@ -9,23 +9,18 @@ let programming = {
 
 // 1. Crie uma função para adicionar uma nova linguagem à chave
 // "languages".
-const addLanguage = (obj, language) => {
-  obj.languages.push(language);
-};
+const addLanguage = (obj, language) => obj.languages.push(language);
 // addLanguage(programming, "Rafael");
 
 // 2. Crie uma função para alterar o valor do nível de dificuldade
 // ("difficulty").
-const changeDifficulty = (obj, newDifficulty) => {
-  obj.difficulty = newDifficulty;
-};
+const changeDifficulty = (obj, newDifficulty) =>
+  (obj.difficulty = newDifficulty);
 // changeDifficulty(programming, 4);
 
 // 3. Crie uma função para excluir uma chave do objeto.
 
-const deleteKey = (obj, keyToDelete) => {
-  delete obj[keyToDelete];
-};
+const deleteKey = (obj, keyToDelete) => delete obj[keyToDelete];
 //deleteKey(programming, "difficulty");
 
 // 4. Crie uma função para adicionar uma nova chave. Faça um teste
